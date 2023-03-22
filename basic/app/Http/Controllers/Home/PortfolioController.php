039821880442e05dc891da09239bebebf4d72333
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-03-22 14:36:20 +0300

    68-

--- basic/app/Http/Controllers/Home/PortfolioController.php
+++ basic/app/Http/Controllers/Home/PortfolioController.php
@@ -126,4 +126,10 @@ class PortfolioController extends Controller
 
     }// End Method
 
+    public function PortfolioDetails($id){
+
+        $portfolio = Portfolio::findOrFail($id);
+        return view('frontend.portfolio_details',compact('portfolio'));
+    }// End Method
+
 }
