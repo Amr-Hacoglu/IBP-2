9c3e47b4b4e9dd3f00e989ca19b716f244c58676
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-03-23 21:18:04 +0300

    74,75-

--- basic/resources/views/admin/admin_master.blade.php
+++ basic/resources/views/admin/admin_master.blade.php
@@ -28,7 +28,8 @@
     <!-- App Css-->
     <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css" >
+
 </head>
 
 <body data-topbar="dark">
@@ -130,5 +131,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
     <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js" ></script>
 
 </html>
--- basic/routes/web.php
+++ basic/routes/web.php
@@ -101,7 +101,8 @@ Route::controller(BlogCategoryController::class)->group(function () {
 // Blog All Route
 Route::controller(BlogController::class)->group(function () {
     Route::get('/all/blog', 'AllBlog')->name('all.blog');
-
+    Route::get('/add/blog', 'AddBlog')->name('add.blog');
+    Route::post('/store/blog', 'StoreBlog')->name('store.blog');
 });
 
 require __DIR__.'/auth.php';
