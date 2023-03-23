396ad04700a9c1cfa994df26b12e6e63a0861844
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-03-23 22:58:11 +0300

    81-

--- basic/app/Http/Controllers/Home/BlogController.php
+++ basic/app/Http/Controllers/Home/BlogController.php
@@ -131,4 +131,13 @@ class BlogController extends Controller
 
     } // End Method
 
+    public function CategoryBlog($id){
+
+        $blogpost = Blog::where('blog_category_id',$id)->orderBy('id','DESC')->get();
+        $allblogs = Blog::latest()->limit(5)->get();
+        $categories = BlogCategory::orderBy('blog_category','ASC')->get();
+        return view('frontend.cat_blog_details',compact('blogpost','allblogs','categories'));
+
+     } // End Method 
+
 }
