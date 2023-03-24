999dda59a1709c4853fc55637cc5d0f61623c6e0
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-03-25 01:20:38 +0300

    92*

--- basic/app/Http/Controllers/Demo/DemoController.php
+++ basic/app/Http/Controllers/Demo/DemoController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
+
+    public function HomeMain(){
+        return view('frontend.index');
+    }// end mehtod 
+
+
     public function Index(){
         return view('about');
     }//and method
